79f426a6f04e83e5a68ad9aea5236ce76233069e
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2017-11-30 15:21:06 +0530

    added views

---
+++ app/src/main/java/com/example/tchavan/examplesampleprogramsjava/ReverseString.java
@@ -0,0 +1,13 @@
+package com.example.tchavan.examplesampleprogramsjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReverseString extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reverse_string);
+    }
+}
